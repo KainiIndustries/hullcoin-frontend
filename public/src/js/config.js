7834f6baffb38c18c10b734524ec7dc4ccee6d79
KainiIndustries <kainihullcoin@gmail.com>
2015-11-17 12:42:17 +0000

    Hullcoin: Update HTML views

--- public/src/js/config.js
+++ public/src/js/config.js
@@ -5,7 +5,7 @@ angular.module('insight').config(function($routeProvider) {
   $routeProvider.
     when('/block/:blockHash', {
       templateUrl: '/views/block.html',
-      title: 'Bitcoin Block '
+      title: 'Hullcoin Block '
     }).
     when('/block-index/:blockHeight', {
       controller: 'BlocksController',
@@ -13,7 +13,7 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/tx/:txId/:v_type?/:v_index?', {
       templateUrl: '/views/transaction.html',
-      title: 'Bitcoin Transaction '
+      title: 'Hullcoin Transaction '
     }).
     when('/', {
       templateUrl: '/views/index.html',
@@ -21,15 +21,15 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/blocks', {
       templateUrl: '/views/block_list.html',
-      title: 'Bitcoin Blocks solved Today'
+      title: 'Hullcoin Blocks solved Today'
     }).
     when('/blocks-date/:blockDate/:startTimestamp?', {
       templateUrl: '/views/block_list.html',
-      title: 'Bitcoin Blocks solved '
+      title: 'Hullcoin Blocks solved '
     }).
     when('/address/:addrStr', {
       templateUrl: '/views/address.html',
-      title: 'Bitcoin Address '
+      title: 'Hullcoin Address '
     }).
     when('/status', {
       templateUrl: '/views/status.html',
